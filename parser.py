1a326ee49f2561cddbfece7c2a8df7e967f07a73
LaSauceBolo <matthieu.deloget@gmail.com>
2025-03-17 10:19:09 +0100

    Ajout d'un __main__ pour écrire deux fichier txt contenant le dico des phonèmes et la trad des mots

--- parser.py
+++ parser.py
@@ -7,19 +7,20 @@ def get_data_from_file(path_file: str):
 
     with open(path_file, 'r', encoding='utf-8') as file:
         for line in file:
-            if line.strip() == '':
+            if line.strip("\n\r ") == '':
                 continue
 
             word, phonegrammes = line.split(' :: ')
-            
-            phonegrammes = phonegrammes.strip("\n\r").split(' ')
+
+            phonegrammes = phonegrammes.strip().split()
 
             tr = []
             for p in phonegrammes:
                 if p not in phonemes_dict.keys():
-                    phonemes_dict[p] = len(phonemes_dict)+1
+                    phonemes_dict[p] = len(phonemes_dict)
                 
                 tr.append(phonemes_dict[p])
+
             
             mots[word] = tr
         
@@ -30,4 +31,12 @@ def get_data_from_file(path_file: str):
 if __name__ == "__main__":
     dico_mots, dico_phonemes = get_data_from_file('test.steno.txt')
 
-    print(dico_phonemes)
+    with open('dico_phonegraphes.txt', 'w', encoding='utf-8') as file:
+        for key, value in dico_phonemes.items():
+            file.write(f"{key} :: {value}\n")
+    
+    with open('test.steno.as_indices.txt', 'w', encoding='utf-8') as file:
+        for key, value in dico_mots.items():
+            value = ' '.join(map(str, value))
+            file.write(f"{key} :: {value}\n")
+    
